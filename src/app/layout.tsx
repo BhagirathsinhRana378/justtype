7b8b11df4b5574950fc63cc440f9a89adf94e6b0
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-12 17:31:07 +0530

    deployment edits

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Cormorant_Garamond } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import Script from "next/script";
